a8ed0a96ff9ad8e041124596ae95b8385fbf8af7
Jonas Reycian B. Saraosos <jsaraosos@fullscale.io>
2022-10-05 23:37:27 +0800

    Adjusted the datatype

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -10,7 +10,7 @@ namespace StarChart.Models
         public int Id { get; set; }
 
         [Required]
-        public int Name { get; set; }
+        public string Name { get; set; }
 
         public int? OrbitedObjectId { get; set; }
 
